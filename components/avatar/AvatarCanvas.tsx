0246bb6a5b9a222a217fae1795bb8e02f57edd8b
si.pei <si.pei@outlook.com>
2026-08-19 10:48:13 -0700

    Fix avatar loading compatibility

--- components/avatar/AvatarCanvas.tsx
+++ components/avatar/AvatarCanvas.tsx
@@ -1,6 +1,5 @@
 "use client";
-import dynamic from "next/dynamic";
 import { Suspense } from "react";
 import { AvatarFallback } from "./AvatarFallback";
-const Scene=dynamic(()=>import("./AvatarScene"),{ssr:false,loading:()=> <AvatarFallback/>});
+import Scene from "./AvatarScene";
 export function AvatarCanvas(){return <Suspense fallback={<AvatarFallback/>}><Scene/></Suspense>}
